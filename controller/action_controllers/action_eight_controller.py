accc106d6b903581d58f665cb7579be53599b5f1
Daniel Luis <daluisgarcia@gmail.com>
2022-06-07 21:07:52 -0400

    Action eight error solved

--- controller/action_controllers/action_eight_controller.py
+++ controller/action_controllers/action_eight_controller.py
@@ -1,4 +1,3 @@
-from datetime import time
 from controller.action_controller import ActionController
 from model.competitor_info import CompetitorInfo
 from model.competitors_list import CompetitorsList
@@ -7,6 +6,8 @@ from model.competitors_list import CompetitorsList
 class ActionEightController(ActionController):
     
     def execute(self):
-        winner: CompetitorInfo = CompetitorsList.competitors_list.sort(key=lambda comp: comp.final_time)[0]
+        competitors_list = CompetitorsList.competitors_list
+        competitors_list.sort(key=lambda comp: comp.final_time)
+        winner: CompetitorInfo = competitors_list[0]
         
         print(f'El ganador general es: {winner.get_full_name()} con un tiempo de: {winner.final_time}')
